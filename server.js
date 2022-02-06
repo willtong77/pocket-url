8d501bb62ee70e99a9df850579fb896305712433
will tong <46694175+willtong77@users.noreply.github.com>
2022-02-05 19:04:09 -0500

    added build script

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
+
 app.use(cors());
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(port);
